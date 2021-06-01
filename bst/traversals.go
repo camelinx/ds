be7a87458f8ff2ccb6d2cb1ca6ad9d42b31213ea
Sudarsan Raghavan <camelinx@gmail.com>
2021-05-31 19:32:00 -0700

    Added support for depth and bread first traversals

--- bst/traversals.go
+++ bst/traversals.go
@@ -202,3 +202,108 @@ func ( tree *Bst_t )PostOrderTraversal( callback BstNodeCallback )( err error ) 
 
     return nil
 }
+
+func ( tree *Bst_t )DepthFirstTraversal( callback BstNodeCallback )( err error ) {
+    if nil == tree || nil == tree.Root {
+        return fmt.Errorf( "invalid tree" )
+    }
+
+    stack := stack_impl.InitWithHandlers(
+        tree.Ctx,
+        stack_impl.Rlock( tree.Rlock_handler ),
+        stack_impl.Runlock( tree.Runlock_handler ),
+        stack_impl.Wlock( tree.Wlock_handler ),
+        stack_impl.Unlock( tree.Unlock_handler ),
+    )
+
+    if nil == stack {
+        return fmt.Errorf( "failed to initialize" )
+    }
+
+    stack.Push( tree.Root )
+
+    for !stack.IsEmpty( ) {
+        pop, err := stack.Peek( )
+        if err != nil {
+            return fmt.Errorf( "corrupted stack" )
+        }
+
+        top, ok := pop.( *BstNode_t )
+        if !ok {
+            return fmt.Errorf( "corrupted tree" )
+        }
+
+        if nil != top.Left {
+            stack.Push( top.Left )
+            continue
+        }
+
+        if nil != top.Right {
+            stack.Push( top.Right )
+            continue
+        }
+
+        stack.Pop( )
+        if nil != callback {
+            err := callback( tree.Ctx, top.Value )
+            if nil != err {
+                return fmt.Errorf( "terminating: callback error = %v", err )
+            }
+        }
+
+        for !stack.IsEmpty( ) {
+            pop, err := stack.Peek( )
+            if err != nil {
+                return fmt.Errorf( "corrupted stack" )
+            }
+
+            next, ok := pop.( *BstNode_t )
+            if !ok {
+                return fmt.Errorf( "corrupted tree" )
+            }
+
+            if top == next.Left && nil != next.Right {
+                stack.Push( next.Right )
+                break
+            }
+
+            top = next
+            stack.Pop( )
+        }
+    }
+
+    return nil
+}
+
+func ( tree *Bst_t )BreadthFirstTraversal( callback BstNodeCallback )( err error ) {
+    if nil == tree || nil == tree.Root {
+        return fmt.Errorf( "invalid tree" )
+    }
+
+    curLevel  := [ ]*BstNode_t{ tree.Root }
+    nextLevel := [ ]*BstNode_t{ }
+
+    for len( curLevel ) > 0 {
+        for _, node := range curLevel {
+            if nil != callback {
+                err := callback( tree.Ctx, node.Value )
+                if nil != err {
+                    return fmt.Errorf( "terminating: callback error = %v", err )
+                }
+            }
+
+            if nil != node.Left {
+                nextLevel = append( nextLevel, node.Left )
+            }
+
+            if nil != node.Right {
+                nextLevel = append( nextLevel, node.Right )
+            }
+        }
+
+        curLevel  = nextLevel
+        nextLevel = [ ]*BstNode_t{ }
+    }
+
+    return nil
+}
